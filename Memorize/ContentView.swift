ec00064d2a5f7f2ed26709bb6679387aecd8f0a3
Kirill <frolkv@gmail.com>
2022-05-15 19:16:27 +0300

    v 0.1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,25 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis = ["✈️", "🚀", "🚁", "🛶", "🚟", "🚇", "⛵️","⛴", "🚕", "🚎", "🚛" ,"🚜", "🛴" ,"🛵" ,"🏍", "🚍", "🚠", "🚞", "🚅", "🛸", "🎠", "🚒", "🏎", "🛻"]
+    
+    @State var emojiCount = 24
+    
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                shuffle
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding()
         }
         .padding(.horizontal)
-        .foregroundColor(.orange)
+    }
+    var shuffle: some View {
+        Button {
+           
+        } label: {
+            Text("Shuffle")
+        }
+    }
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    var add: some View {
+        Button{
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
 struct CardView: View {
+    var content: String
+    
+    @State var isFaceUp = true
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("✈️")
-                .font(.largeTitle)
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
@@ -35,5 +89,8 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
